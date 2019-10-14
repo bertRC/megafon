5533a05c72ee8ea5a491d8ad55d13d6f2f664143
bertRC <sithalbert@ya.ru>
2019-10-14 22:53:16 +0300

    Добавлен переопределнный метод toString

--- src/main/java/ru/itpark/model/TariffForDevices.java
+++ src/main/java/ru/itpark/model/TariffForDevices.java
@@ -23,4 +23,29 @@ public class TariffForDevices extends AbstractTariff {
     public int getMegabytesCost() {
         return megabytesCost;
     }
+
+    @Override
+    public String toString() {
+        String rubleSymbol = " \u20BD ";
+        String endOfLine = "\r\n";
+        String result = super.getTitle();
+        if (super.isHit()) {
+            result = result + " (Хит)";
+        }
+        result = result + endOfLine + description + endOfLine;
+        if ((megabytes == 0) || (megabytesCost == 0)) {
+            result = result + "Безлимитный интернет" + endOfLine;
+        } else {
+            result = result + "Оплата трафика по факту использования" + endOfLine;
+        }
+        result = result + super.getPrice() + rubleSymbol;
+        if (super.getPrice() == 0) {
+            result = result + super.getPayPeriod();
+        }
+        result = result + endOfLine;
+        if (super.isCanBeOrderedOnline()) {
+            result = result + "*Выбрать*" + endOfLine;
+        }
+        return result;
+    }
 }
