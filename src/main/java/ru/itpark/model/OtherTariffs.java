bd1b2dfec97bd7fc312ebea8847a37dfa1dc95b3
bertRC <sithalbert@ya.ru>
2019-10-14 23:38:32 +0300

    Добавлен переопределенный метод toString

--- src/main/java/ru/itpark/model/OtherTariffs.java
+++ src/main/java/ru/itpark/model/OtherTariffs.java
@@ -23,4 +23,27 @@ public class OtherTariffs extends AbstractTariff {
     public String getCallDescription() {
         return callDescription;
     }
+
+    @Override
+    public String toString() {
+        String rubleSymbol = " \u20BD ";
+        String endOfLine = "\r\n";
+        String result = super.getTitle();
+        if (super.isHit()) {
+            result = result + " (Хит)";
+        }
+        result = result + endOfLine + description + endOfLine + internetDescription + endOfLine;
+        if (callDescription != null) {
+            result = result + callDescription + endOfLine;
+        }
+        result = result + super.getPrice() + rubleSymbol;
+        if (super.getPayPeriod() != null) {
+            result = result + super.getPayPeriod();
+        }
+        result = result + endOfLine;
+        if (super.isCanBeOrderedOnline()) {
+            result = result + "*Выбрать*" + endOfLine;
+        }
+        return result;
+    }
 }
