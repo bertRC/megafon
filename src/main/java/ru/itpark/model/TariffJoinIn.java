7bfd5811824d68410eab56bb040a7967b32f8bc7
bertRC <sithalbert@ya.ru>
2019-10-14 20:56:57 +0300

    Добавлены уникальные для класса TariffJoinIn поля, конструктор и геттеры

--- src/main/java/ru/itpark/model/TariffJoinIn.java
+++ src/main/java/ru/itpark/model/TariffJoinIn.java
@@ -1,5 +1,38 @@
 package ru.itpark.model;
 
 public class TariffJoinIn extends AbstractTariff {
+    private int minutes;
+    private String minutesDescription;
+    private int gigabytes;
+    private int smsCount;
+    private String[] additionalFeatures;
 
+    public TariffJoinIn(int id, String title, int price, String payPeriod, boolean isHit, boolean canBeOrderedOnline, int minutes, String minutesDescription, int gigabytes, int smsCount, String[] additionalFeatures) {
+        super(id, title, price, payPeriod, isHit, canBeOrderedOnline);
+        this.minutes = minutes;
+        this.minutesDescription = minutesDescription;
+        this.gigabytes = gigabytes;
+        this.smsCount = smsCount;
+        this.additionalFeatures = additionalFeatures;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public String getMinutesDescription() {
+        return minutesDescription;
+    }
+
+    public int getGigabytes() {
+        return gigabytes;
+    }
+
+    public int getSmsCount() {
+        return smsCount;
+    }
+
+    public String[] getAdditionalFeatures() {
+        return additionalFeatures;
+    }
 }
