4f96b03e398102dd58d5babb2d35bc8ab79abf29
bertRC <sithalbert@ya.ru>
2019-10-14 21:13:26 +0300

    Создан первый объект-тариф

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,7 +1,23 @@
 package ru.itpark;
 
+import ru.itpark.model.AbstractTariff;
+import ru.itpark.model.TariffJoinIn;
+
 public class Main {
     public static void main(String[] args) {
+        AbstractTariff[] tariffs = new AbstractTariff[6];
 
+        tariffs[0] = new TariffJoinIn(
+                1,
+                "Включайся! Общайся",
+                350,
+                "за 30 дней",
+                true,
+                true,
+                600,
+                "Звонки на все мобильные номера России",
+                0,
+                0,
+                new String[2]);
     }
 }
